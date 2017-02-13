3d47ffdc128a0b73045ad48aedd69527387a2c9d
Michael Fryer <michael.d.fryer@gmail.com>
2017-02-13 12:36:03 -0700

    Most stuff fixed. Click still borked.

--- app/routes/main.js
+++ app/routes/main.js
@@ -1,15 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  doDarkenContent: false,
-
   model() {
-    return Ember.$.getJSON('https://www.reddit.com/r/pcmasterrace+rarepuppers+watches+me_irl/.json');
-  },
-  actions: {
-    darkenContent() {
-      this.set('doDarkenContent', !this.get('doDarkenContent'));
-      console.log(this.get('doDarkenContent'));
-    }
+    return Ember.$.getJSON('https://www.reddit.com/r/pcmasterrace+rarepuppers+buildapc+watches+me_irl/.json');
   }
 });
